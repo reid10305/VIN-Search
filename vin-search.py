8510bb39b324e8cebafe27ade4004a57c6865228
Daniel Pifer <42287892+reid10305@users.noreply.github.com>
2023-08-16 20:38:51 -0500

    Update vin-search.py

--- vin-search.py
+++ vin-search.py
@@ -1,46 +1,46 @@
 import pandas as pd
-import os
 import glob
-import json
+from tqdm import tqdm
 
 def main(transfer_list, vin_list):
     transfer_df = pd.read_csv(transfer_list)
     vin_df = pd.read_csv(vin_list)
     
     # create mew fields in transfer file
-    transfer_df['Suggested_VIN'] = transfer_df['VIN']
+    transfer_df['Suggested_VIN'] = ''
     transfer_df['Location'] = ''
-    transfer_df['Suggested_SKU'] = transfer_df['SKU']
+    transfer_df['Suggested_SKU'] = ''
+
+    tqdm.pandas()
+    pbar = tqdm(transfer_df.iterrows(), total=len(transfer_df))
     
     # search the vin list for locations
-    for i in transfer_df['VIN']:
-        i = i.strip()
-        print(i)
-        results = vin_df.index[vin_df[' Number'] == i].to_list()
-        print(results)
+    for i, j in pbar:
+        vin = transfer_df.loc[i, ['VIN']][0]
+        results = vin_df.index[vin_df[' Number'] == vin].to_list()
+        pbar.set_description(vin)
         try:
             # print(vin_df.loc[results[-1], 'Location'])
-            transfer_df.loc[results[-1], 'Location'] = vin_df.loc[results[-1], 'Location']
-            transfer_df.loc[results[-1], 'Suggested_SKU'] = vin_df.loc[results[-1], 'Item']
-            transfer_df.loc[results[-1], 'Suggested_VIN'] = vin_df.loc[results[-1], ' Number']
+            transfer_df.loc[i, 'Location'] = vin_df.loc[results[-1], 'Location']
+            transfer_df.loc[i, 'Suggested_SKU'] = vin_df.loc[results[-1], 'Item']
+            transfer_df.loc[i, 'Suggested_VIN'] = vin_df.loc[results[-1], ' Number']
             
             
         except Exception as e:
-            print(e)
-            new_vin = i.replace('O', '0')
+            new_vin = vin.replace('O', '0')
             results1 = vin_df.index[vin_df[' Number'] == new_vin].to_list()
             try:
-                transfer_df.loc[results1[-1], 'Location'] = vin_df.loc[results1[-1], 'Location']
-                transfer_df.loc[results1[-1], 'Suggested_SKU'] = vin_df.loc[results1[-1], 'Item']
-                transfer_df.loc[results1[-1], 'Suggested_VIN'] = vin_df.loc[results1[-1], ' Number']
+                transfer_df.loc[i, 'Location'] = vin_df.loc[results1[-1], 'Location']
+                transfer_df.loc[i, 'Suggested_SKU'] = vin_df.loc[results1[-1], 'Item']
+                transfer_df.loc[i, 'Suggested_VIN'] = vin_df.loc[results1[-1], ' Number']
             
             except:
-                new_vin1 = i.replace('0', 'O')
+                new_vin1 = vin.replace('0', 'O')
                 results2 = vin_df.index[vin_df[' Number'] == new_vin1].to_list()
                 try:
-                    transfer_df.loc[results2[-1], 'Location'] = vin_df.loc[results2[-1], 'Location']
-                    transfer_df.loc[results2[-1], 'Suggested_SKU'] = vin_df.loc[results2[-1], 'Item']
-                    transfer_df.loc[results2[-1], 'Suggested_VIN'] = vin_df.loc[results2[-1], ' Number']
+                    transfer_df.loc[i, 'Location'] = vin_df.loc[results2[-1], 'Location']
+                    transfer_df.loc[i, 'Suggested_SKU'] = vin_df.loc[results2[-1], 'Item']
+                    transfer_df.loc[i, 'Suggested_VIN'] = vin_df.loc[results2[-1], ' Number']
 
                 except:
                     pass
